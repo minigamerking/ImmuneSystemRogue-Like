745506e18aaca2211a394f9e40abc8d9dfd6d672
MarsHalllow123 <143437996+MarsHalllow123@users.noreply.github.com>
2024-08-19 12:59:57 +0200

    Sprite and projectile changes

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -5,9 +5,13 @@ const SPEED = 900
 
 var direction: Vector2 = Vector2(0,1)
 @export var dmg = 1
+@onready var proj_sprite = $Sprite2D
+
 func _physics_process(delta):
-	
-	
+	if direction.x > 0:
+		proj_sprite.rotation = tan(direction.y)
+	else:
+		proj_sprite.rotation = tan(-direction.y)
 	velocity= SPEED* direction
 	
 	move_and_slide()
