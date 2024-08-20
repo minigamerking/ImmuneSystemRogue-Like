fe4e1a12771b45271708b3285ca5d56172729a00
Geal <139901989+Geal6655@users.noreply.github.com>
2024-08-20 18:35:10 +0430

    replay/quitgame

--- End screen.gd
+++ End screen.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_replay_game_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
